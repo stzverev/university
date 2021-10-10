a3969f2d2778b431ec42620d4bb8831b22e1471c
stzverev <stanislav.zv@yandex.ru>
2021-10-10 14:49:05 +0300

    Added a configuration class

---
+++ src/main/java/ua/com/foxminded/university/data/Config.java
@@ -0,0 +1,24 @@
+package ua.com.foxminded.university.data;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("ua.com.foxminded.university.data")
+@PropertySource("classpath:queries.properties")
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+        return dataSource;
+    }
+
+}
