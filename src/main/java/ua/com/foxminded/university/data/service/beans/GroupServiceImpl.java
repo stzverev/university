d6f5ca9fc3af23c412de66cb5c5dbf2ac15a4f00
stzverev <stanislav.zv@yandex.ru>
2021-11-03 17:05:01 +0300

    Added logs

--- src/main/java/ua/com/foxminded/university/data/service/beans/GroupServiceImpl.java
+++ src/main/java/ua/com/foxminded/university/data/service/beans/GroupServiceImpl.java
@@ -2,7 +2,10 @@ package ua.com.foxminded.university.data.service.beans;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,8 @@ import ua.com.foxminded.university.data.service.GroupService;
 public class GroupServiceImpl implements GroupService {
 
     private GroupDao groupDao;
+    private final Logger logger = LoggerFactory.getLogger(
+            GroupServiceImpl.class);
 
     @Autowired
     public GroupServiceImpl(GroupDao groupDao) {
@@ -25,12 +30,23 @@ public class GroupServiceImpl implements GroupService {
 
     @Override
     public void save(Group group) {
-        groupDao.save(group);
+        try {
+            groupDao.save(group);
+        } catch (Exception e) {
+            logger.error("Error when saving the {}: {}",
+                    group.getName(),
+                    e.getMessage());
+        }
     }
 
     @Override
     public void save(List<Group> groups) {
-        groupDao.save(groups);
+        try {
+            groupDao.save(groups);
+        } catch (Exception e) {
+            logger.error("Error when saving the list of groups: {}",
+                    e.getMessage());
+        }
     }
 
     @Override
@@ -45,12 +61,34 @@ public class GroupServiceImpl implements GroupService {
 
     @Override
     public void addToCourses(Group group) {
-        groupDao.addToCourses(group);
+        try {
+            groupDao.addToCourses(group);
+        } catch (Exception e) {
+            String coursesName = group.getCourses()
+                    .stream()
+                    .map(Course::getName)
+                    .collect(Collectors.joining(", "));
+            logger.error(""
+                    + "Error when adding courses to group:%n"
+                    + "group: {}%n"
+                    + "courses: {}",
+                    group.getName(),
+                    coursesName);
+        }
     }
 
     @Override
     public void removeFromCourse(Group group, Course course) {
-        groupDao.deleteFromCourse(group, course);
+        try {
+            groupDao.deleteFromCourse(group, course);
+        } catch (Exception e) {
+            logger.error(""
+                    + "Error when removing course from group:%n"
+                    + "group: {}%n"
+                    + "course: {}",
+                    group.getName(),
+                    course.getName());
+        }
     }
 
     @Override
