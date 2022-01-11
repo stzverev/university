e78125504e5a138a07b72f46f2f908b3ddeb5e20
stzverev <stanislav.zv@yandex.ru>
2022-01-11 10:36:09 +0300

    Added the Lombok

--- src/main/java/ua/com/foxminded/university/data/model/AbstractEntity.java
+++ src/main/java/ua/com/foxminded/university/data/model/AbstractEntity.java
@@ -9,21 +9,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @MappedSuperclass
+@Getter
+@Setter
+@ToString
 public class AbstractEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     @Override
     public int hashCode() {
--- src/main/java/ua/com/foxminded/university/data/model/Course.java
+++ src/main/java/ua/com/foxminded/university/data/model/Course.java
@@ -9,8 +9,19 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "courses")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Course extends AbstractEntity {
 
     @Column(name = "name")
@@ -20,55 +31,16 @@ public class Course extends AbstractEntity {
     private Set<Teacher> teachers = new HashSet<>();
 
     @ManyToMany(mappedBy = "courses")
+    @ToStringExclude
     private Set<Group> groups = new HashSet<>();
 
     @OneToMany(mappedBy = "course")
+    @ToStringExclude
     private Set<TabletimeRow> tabletime = new HashSet<>();
 
     public Course(String name) {
         super();
         this.name = name;
-    }
-
-    public Course() {
-        super();
-    }
-
-    public Set<TabletimeRow> getTabletime() {
-        return tabletime;
-    }
-
-    public void setTabletime(Set<TabletimeRow> tabletime) {
-        this.tabletime = tabletime;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Set<Teacher> getTeachers() {
-        return teachers;
-    }
-
-    public Set<Group> getGroups() {
-        return groups;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setTeachers(Set<Teacher> teachers) {
-        this.teachers = teachers;
-    }
-
-    public void setGroups(Set<Group> groups) {
-        this.groups = groups;
-    }
-
-    @Override
-    public String toString() {
-        return "Course [name=" + name + ", id=" + getId() + "]";
     }
 
 }
